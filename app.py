9977fe626ceab63cabef701f8d2e53bf1fff5273
Kaushal-13 <84263127+Kaushal-13@users.noreply.github.com>
2022-04-28 12:54:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,66 @@
 import streamlit as st 
+import pandas as pd;
+#do using vader
 from textblob import TextBlob 
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-
-st.write(""" Simple Sentiment Analysis app,
-This app can be usesd to identify people's opinion to some extent""");
-
-# Title
-st.title("Sentiment Analysis")
-
-result_sentiment = 2
-# Sentiment Analysis
-st.subheader("Analyse Your Text")
-
-message = st.text_area("Enter Text")
-if st.button("Analyze"):
-      blob = TextBlob(message)
-      result_sentiment = blob.sentiment[0]
-			
+def score(sentence):
+    obj = SentimentIntensityAnalyzer()
+    sentiment_dict = obj.polarity_scores(sentence);
+    ''' to map the value bw 0 - 5 we use f(x) = 2.5(x+1)'''
+    val = sentiment_dict['compound'] + 1;
+    val = val*2.5;
+    #return val if you need to map bw 0 and 5;
+    return sentiment_dict['compound'];
     
-if result_sentiment > 0.5 and result_sentiment < 1 :
-      st.write('Prediction  :')
-      st.subheader('Positive')
-      st.success(f'Score : {result_sentiment}')
+page = st.selectbox("Choose your page",["About us","Our App Basic","Faqs","Advanced version"]);
 
-elif result_sentiment < 0 :
-      st.write('Prediction  :')
-      st.subheader('Negative')
-      st.success(f'Score : {result_sentiment}')
+if(page == "About us"):
+      st.title("About us");
+      st.write(""" This is a basic version of the sentiment analysis app
+                we are a team of two, Kaushal Tiwari and Shubh Agarwal studying at Nit-Ap.
+                The goal of this app is to track people's opinion """);
+elif(page == "Our App"):
+        # Title
+        st.title("Sentiment Analysis")
+        result_sentiment = 2
+        # Sentiment Analysis
+        st.subheader("Analyse Your Text")
 
-elif result_sentiment >= 0 and result_sentiment <= 0.5 :
-      st.write('Prediction  :')
-      st.subheader('Neutral')
-      st.success(f'Score : {result_sentiment}')
+        message = st.text_area("Enter Text")
+        if st.button("Analyze"):
+              result_sentiment = score(message)
+            
+        if result_sentiment > 0.5 and result_sentiment < 1 :
+              st.write('Prediction  :')
+              st.subheader('Positive')
+              st.success(f'Score : {result_sentiment}')
+
+        elif result_sentiment < 0 :
+              st.write('Prediction  :')
+              st.subheader('Negative')
+              st.success(f'Score : {result_sentiment}')
+
+        elif result_sentiment >= 0 and result_sentiment <= 0.5 :
+              st.write('Prediction  :')
+              st.subheader('Neutral')
+              st.success(f'Score : {result_sentiment}')
+              
+elif (page == "Faqs"):
+      st.title("FAQS");
+      st.write("""What is sentiment analysis? \n
+                According to Google  Sentiment analysis, also referred to as opinion mining, is an approach to natural language processing 
+                (NLP) that identifies the emotional tone behind a body of text. 
+                This is a popular way for organizations to determine and categorize opinions about a product, service, or idea.""");
+                
+      st.write("""How can sentiment anlysis be used?\n
+            Sentiment Analysis can be used to understand public opinions on nearly everything like the review of a product 
+            or the working of a company or the way the people are reacting to news. """);
+elif(page == "Advanced version"):
+      st.title("This is the advanced version It can accept datasets");
+      uploadedFile = st.file_uploader(fileUploadLabel,type=['csv','xlsx'],accept_multiple_files = False,key = "fileUploader");
+      if uploadedFile is not None and uploadedFile.endswith('csv'):
+            st.write('Prediction  :')
+            df1 = pd.read_csv(uploadedFile);
+      if uploadedFile is not None and uploadedFile.endswith('xlsx'):
+            df1 = pd.read_excel(uploadedFile);

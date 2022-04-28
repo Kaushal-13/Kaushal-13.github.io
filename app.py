6f01ad41190eb31fdd24e24adc901a7c9a465755
Kaushal-13 <84263127+Kaushal-13@users.noreply.github.com>
2022-04-28 21:28:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import pandas as pd;
+from streamlit_option_menu import option_menu
 #do using vader
 from textblob import TextBlob 
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
